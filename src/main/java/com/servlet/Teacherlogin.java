b739ba066b1485faa3da06a378f4c11d241add12
SumitKumar748 <skghosh3499@gmail.com>
2024-12-30 20:20:10 +0530

    1st stage complete

---
+++ src/main/java/com/servlet/Teacherlogin.java
@@ -0,0 +1,65 @@
+package com.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+@WebServlet("/teacherlogin")
+public class Teacherlogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Teacherlogin() {
+        super();
+    }
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException 
+	{
+		PrintWriter out= resp.getWriter();
+		 HttpSession session= req.getSession();
+		 String email=req.getParameter("teid");
+		 String password= req.getParameter("tpwd");
+		 
+		 try 
+		 {
+			 Class.forName("com.mysql.cj.jdbc.Driver");
+			 Connection con= DriverManager.getConnection("jdbc:mysql://localhost:3306/dynamicproject","root","sumit@4070");
+			 PreparedStatement pst=con.prepareStatement("select * from technoindiateachers where Email=? and Password=?");
+			 pst.setString(1, email);
+			 pst.setString(2, password);
+			 ResultSet rslt=pst.executeQuery();
+					 if(rslt.next()) 
+					 {
+						 RequestDispatcher rd= req.getRequestDispatcher("/teacherwelcome.jsp");
+						 rd.forward(req, resp);
+						 
+					 }
+					 else {
+//						 		resp.setContentType("text/html");
+//						 		out.print("<h2 style=\"color:red;text-align:center;\">Invalid Details</h2>");
+				         		
+						 		req.setAttribute("msg", "Invalid Details");
+						 		RequestDispatcher rd= req.getRequestDispatcher("/tlogin.jsp");
+				         		rd.include(req,resp);
+						 
+					 	}
+			 
+		 }
+		 catch(Exception e) 
+		 {
+			 e.printStackTrace();
+		 }
+	
+
+	}
+}
